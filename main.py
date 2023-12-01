05a4caffcee3bea14be2164e764d1fd1bb026cc5
Talha Mehboob <39377184+talhamehboob10@users.noreply.github.com>
2023-11-30 23:46:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import os
+import tom_architecture
+import tom_simulator
+
+
+if __name__ == "__main__":
+    if os.path.exists("simulation_log.txt"):
+        os.remove("simulation_log.txt")
+    processor = tom_architecture.TomasuloProcessor()
+    simulator = tom_simulator.TomasuloSimulator(processor)
+    simulator.run_simulation()
+
